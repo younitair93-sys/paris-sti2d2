312693f267446a43611653d8cfc9e524515a5ba5
younitair93-sys <younitair93@gmail.com>
2026-05-04 23:44:13 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,14 @@
 import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 
-// Configure Firebase avec tes identifiants
 const firebaseConfig = {
-  apiKey: "REMPLACE_MOI",
-  authDomain: "REMPLACE_MOI",
-  databaseURL: "https://REMPLACE_MOI-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "REMPLACE_MOI",
-  storageBucket: "REMPLACE_MOI",
-  messagingSenderId: "REMPLACE_MOI",
-  appId: "REMPLACE_MOI"
+  apiKey: "AIzaSyDeL7O4yquTomNgfHqwBwaTfK1C2vvrDYE",
+  authDomain: "paris-sti2d2.firebaseapp.com",
+  databaseURL: "https://paris-sti2d2-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "paris-sti2d2",
+  storageBucket: "paris-sti2d2.firebasestorage.app",
+  messagingSenderId: "689887286589",
+  appId: "1:689887286589:web:b6ac19c9adfa641d0de154"
 };
 
 const app = initializeApp(firebaseConfig);
